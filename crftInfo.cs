e25ee195ced09493be90d096fd68a82338cfa266
jmuozan <146674258+jmuozan@users.noreply.github.com>
2025-04-12 09:57:59 +0200

    first commit

---
+++ crftInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace crft
+{
+  public class crftInfo : GH_AssemblyInfo
+  {
+    public override string Name => "crft Info";
+
+    //Return a 24x24 pixel bitmap to represent this GHA library.
+    public override Bitmap Icon => null;
+
+    //Return a short string describing the purpose of this GHA library.
+    public override string Description => "";
+
+    public override Guid Id => new Guid("7addb38a-ab0d-4229-9e50-e7396787b462");
+
+    //Return a string identifying you or your company.
+    public override string AuthorName => "";
+
+    //Return a string representing your preferred contact details.
+    public override string AuthorContact => "";
+
+    //Return a string representing the version.  This returns the same version as the assembly.
+    public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+  }
+}
