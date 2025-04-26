1b684082e4589a26a0ddc44a5a413f869c32ed2c
jmuozan <146674258+jmuozan@users.noreply.github.com>
2025-04-26 20:53:19 +0200

    errors solved

--- obj/Debug/net7.0/osx-arm64/crft.AssemblyInfo.cs
+++ obj/Debug/net7.0/osx-arm64/crft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of crft")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b83000ec2419277cb5fa709528d4d3b1ec80dceb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fb98e869ba12a8905cce045f727036967a27d4b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("crft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
