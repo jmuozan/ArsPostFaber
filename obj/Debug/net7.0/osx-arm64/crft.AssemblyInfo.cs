a389440463f25274a895470ac8c9d512abb107a8
jmuozan <146674258+jmuozan@users.noreply.github.com>
2025-05-03 16:46:47 +0200

    debug

--- obj/Debug/net7.0/osx-arm64/crft.AssemblyInfo.cs
+++ obj/Debug/net7.0/osx-arm64/crft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of crft")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a5c6925d3338bb8aea14d6de46be404e889ea80e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+35f04fd415e62c90466f3a8db682f66740ffb7de")]
 [assembly: System.Reflection.AssemblyProductAttribute("crft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
