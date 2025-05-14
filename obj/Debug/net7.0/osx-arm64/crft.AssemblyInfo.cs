fe5eb313b1478bbcda541577e480ee4a65cc0981
jmuozan <146674258+jmuozan@users.noreply.github.com>
2025-05-14 17:36:35 +0200

    openai component generation working

--- obj/Debug/net7.0/osx-arm64/crft.AssemblyInfo.cs
+++ obj/Debug/net7.0/osx-arm64/crft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of crft")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fd651f4744f8331471b2063a3ac5b980da9e5266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d2f5e611e6f71e899364cfb88ae430336d053a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("crft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
